50b69ea3a95227affd383b8cd83933251557b533
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-05-16 06:23:32 +0600

    implemented moduler pattern

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { type Application, type Request, type Response } from "express"
 
 import { pool } from "./db"
+import { userRoute } from "./modules/user/user.route"
 const app: Application = express()
 
 app.use(express.json())
@@ -16,27 +17,9 @@ app.get("/", (req: Request, res: Response) => {
     })
 })
 
-app.post("/api/users", async (req: Request, res: Response) => {
-    try {
-        const { name, email, password, age } = req.body
-        const result = await pool.query(`
-        INSERT INTO users(name,email,password,age)
-        VALUES ($1,$2,$3, $4)
-        RETURNING *
-        `, [name, email, password, age])
-        res.status(200).json({
-            success: true,
-            message: "user created successfully",
-            data: result.rows[0]
-        })
-    } catch (error: any) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-            error: error
-        })
-    }
-})
+app.use("/api/users", userRoute)
+
+
 
 app.get("/api/users", async (req: Request, res: Response) => {
     try {
---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,23 @@
+import type { Request, Response } from "express"
+import { pool } from "../../db"
+import { userService } from "./user.service"
+
+const createUser = async (req: Request, res: Response) => {
+    try {
+        const result = await userService.createUserIntoDB(req.body)
+        res.status(200).json({
+            success: true,
+            message: "user created successfully",
+            data: result.rows[0]
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
+}
+export const userController = {
+    createUser,
+}
