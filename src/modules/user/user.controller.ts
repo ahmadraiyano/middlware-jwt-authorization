586a4f83359748b2c4451846ccb6c5d4423df98e
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-05-16 17:29:13 +0600

    structured in modules architechture

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -18,6 +18,103 @@ const createUser = async (req: Request, res: Response) => {
         })
     }
 }
+
+const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const result = await userService.getAllUsersFromDB()
+        res.status(200).json({
+            success: true,
+            message: "users data retrieved successfully",
+            data: result.rows
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
+}
+
+const getSingleUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const result = await userService.getSingleUserFromDB(id as string)
+        // console.log(result);
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "users data does not exist"
+            })
+        }
+        // console.log(result);
+        res.status(200).json({
+            success: true,
+            message: "users data retrieved successfully",
+            data: result.rows
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
+}
+
+const updateUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const result = await userService.updateUserToDB(req.body, id as string)
+        // console.log(result);
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "users data does not exist"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "users data updated successfully",
+            data: result.rows[0]
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
+}
+
+const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const result = await userService.deleteUserFromDB(id as string)
+        // console.log(result);
+        if (result.rowCount === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "users data does not exist"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "users data deleted successfully"
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
+}
+
 export const userController = {
     createUser,
+    getAllUsers,
+    getSingleUser,
+    updateUser,
+    deleteUser
 }
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,9 +1,16 @@
-import { Router, type Request, type Response } from "express";
-import { pool } from "../../db";
+import { Router} from "express";
 import { userController } from "./user.controller";
 
 const router = Router()
 
 router.post("/", userController.createUser)
+
+router.get("/", userController.getAllUsers)
+
+router.get("/:id", userController.getSingleUser )
+
+router.put("/:id", userController.updateUser)
+
+router.delete("/:id", userController.deleteUser)
 
 export const userRoute = router
