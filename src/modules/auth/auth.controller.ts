bfbbbb46e50d45171b299602de666551626a4512
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-05-19 17:22:49 +0600

    secured refresh token api

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,32 +1,51 @@
 import type { Request, Response } from "express";
 import { authService } from "./auth.service";
 
-const createLogin = async (req: Request, res: Response) =>{
-try {
-    const result = await authService.createLoginIntoDB(req.body)
+const createLogin = async (req: Request, res: Response) => {
+    try {
+        const result = await authService.createLoginIntoDB(req.body)
 
-    const {refreshToken} = result
+        const { refreshToken } = result
 
-    res.cookie("refreshToken", refreshToken, {
-        secure: false, // true in production
-        httpOnly: true,
-        sameSite: "lax"
-    })
+        res.cookie("refreshToken", refreshToken, {
+            secure: false, // true in production
+            httpOnly: true,
+            sameSite: "lax"
+        })
 
-    res.status(200).json({
-        success: true,
-        message: "user logged in successfully",
-        data: result
-    })
-} catch (error: any) {
-    res.status(500).json({
-        status: false,
-        message: error.message,
-        error: error
-    })
+        res.status(200).json({
+            success: true,
+            message: "user logged in successfully",
+            data: result
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            status: false,
+            message: error.message,
+            error: error
+        })
+    }
 }
+
+const refreshToken = async (req: Request, res: Response) => {
+    try {
+        const result = await authService.generateRefreshToken(req.cookies.refreshToken)
+
+        res.status(200).json({
+            success: true,
+            message: "access token generated",
+            data: result
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            status: false,
+            message: error.message,
+            error: error
+        })
+    }
 }
 
 export const authController = {
-    createLogin
+    createLogin,
+    refreshToken
 }
