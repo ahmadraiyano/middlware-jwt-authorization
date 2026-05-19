ab5a379e1c357d0946e8b81d6377125fa5eebe81
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-05-19 11:18:24 +0600

    created logger middleware that generates log in logger txt file

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,21 @@ import express, { type Application, type Request, type Response } from "express"
 import { userRoute } from "./modules/user/user.route"
 import { profileRoute } from "./modules/profile/profile.route"
 import { authRoute } from "./modules/auth/auth.route"
+import fs from "fs"
 const app: Application = express()
 
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
+
+app.use((req,res,next)=>{
+    console.log("date", Date.now() , "\nurl", req.url, "\nmethod", req.method);
+    const log = `\nmethod -> ${req.method}, time -> ${Date.now()}, URL -> ${req.url}\n`
+    fs.appendFile("logger.txt", log,(err)=>{
+        console.log(err);
+    })
+    next()
+})
 
 
 
